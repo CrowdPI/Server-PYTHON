32db86de9c879382e09aebb2dbca5e39d6bde3a0
MrT <turgeon.dev@gmail.com>
2024-09-09 22:28:37 -0600

    Added PRODUCT_INGREDIENTS Table

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, DateTime, String, Integer, func
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -11,6 +12,9 @@ class Product(Base):
     brand_name = Column(String(60), unique=True, nullable=True)
     name = Column(String(60), unique=True, nullable=False)
 
+    # RELATIONSHIP(s) 
+    ingredients = relationship('Product_Ingredients', back_populates='product')
+
     def __repr__(self):
         return f'id: {self.id} - brand_name: {self.brand_name} > name: {self.name}'
 
@@ -21,5 +25,22 @@ class Ingredient(Base):
     name = Column(String(60), unique=True, nullable=False)
     alias = Column(String(60), unique=True, nullable=True)
 
+    # RELATIONSHIP(s)
+    products = relationship('Product_Ingredients', back_populates='ingredient')
+
     def __repr__(self):
         return f'id: {self.id} - name: {self.name}'
+
+class Product_Ingredients(Base):
+    __tablename__ = 'product_ingredients'
+
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
+    ingredient_id = Column(Integer, ForeignKey('ingredients.id'), nullable=False)
+
+    # Establish relationships
+    product = relationship('Product', back_populates='ingredients')
+    ingredient = relationship('Ingredient', back_populates='products')
+
+    def __repr__(self):
+        return f'id: {self.id} - product_id: {self.product_id} - ingredient_id: {self.ingredient_id}'
