3f391969a0fdf7fd1603010215200ca4c1c7209a
MrT <turgeon.dev@gmail.com>
2024-09-09 18:03:25 -0600

    CORS

--- consts/ingredients.py
+++ consts/ingredients.py
@@ -4,5 +4,4 @@ INGREDIENTS = [
         "name": "Sugar",
         "summary": None
     },
-    
 ]
--- index.py
+++ index.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 # CONSTS
 from consts.ingredients import INGREDIENTS
 from version import CHANGE_LOG
 
 server = Flask(__name__)
+CORS(server, resources={r"/*": {"origins": "*"}})  # Allow all origins
+# CORS(server, resources={r"/*": {"origins": ["...","..."]}})  # Allow all origins
 
 @server.route('/', methods=['GET'])
 def hello_world():
@@ -31,4 +35,5 @@ def get_ingredient(id):
     return jsonify(ingredient), 200
 
 if __name__ == '__main__':
-    server.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    server.run(host='0.0.0.0', port=port, debug=True)
