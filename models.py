0c3a63bf6e7f00d16a64eb18bf4e800643676fe0
MrT <turgeon.dev@gmail.com>
2024-09-09 22:25:46 -0600

    Added PRODUCTS Table

--- models.py
+++ models.py
@@ -4,6 +4,16 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 metadata = Base.metadata
 
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    brand_name = Column(String(60), unique=True, nullable=True)
+    name = Column(String(60), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'id: {self.id} - brand_name: {self.brand_name} > name: {self.name}'
+
 class Ingredient(Base):
     __tablename__ = 'ingredients'
 
